1ceba8bbbc166f550bd850098ae12f3cf1d7dbd0
SauBanh <saubanh05062001@gmail.com>
2023-04-15 18:37:55 +0700

    useLoaderData

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,50 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
-const DUMMY_EVENTS = [
-    {
-        id: 'e1',
-        title: 'GAMING VIETNAM 2023 - Triển lãm quốc tế về game và trò chơi điện tử',
-    },
-    {
-        id: 'e2',
-        title: 'ICTCOMM 2023 – Triển lãm Quốc tế ngành CNTT – Điện tử viễn thông – Gaming',
-    },
-    {
-        id: 'e3',
-        title: 'LEDTEC ASIA 2023 – Triển lãm LED/OLED, thiết bị chiếu sáng, bảng quảng cáo KTS',
-    },
-    {
-        id: 'e4',
-        title: 'EMA VIETNAM 2023 – Triển lãm quốc tế ngành Điện – Máy Móc Thiết Bị Công Nghiệp – Tự Động Hóa',
-    },
-    {
-        id: 'e5',
-        title: 'VIETNAM TELEFILM 2023 – Triển lãm Phim & Truyền Hình Việt Nam',
-    },
-    {
-        id: 'e6',
-        title: 'VIMF 2023 – Triển Lãm Công Nghiệp Và Sản Xuất Việt Nam',
-    },
-    {
-        id: 'e7',
-        title: 'AV SHOW HCM 2023 – Triển Lãm Thiết Bị Nghe Nhìn tại TP. HCM',
-    },
-];
+import EventsList from '../components/EventsList';
 
 function Events() {
-    return (
-        <>
-            <h1>Events Page</h1>
-            <ul>
-                {DUMMY_EVENTS.map((event) => (
-                    <li key={event.id}>
-                        <Link to={event.id}>{event.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [fetchedEvents, setFetchedEvents] = useState();
+    // const [error, setError] = useState();
+
+    // useEffect(() => {
+    //     async function fetchEvents() {
+    //         setIsLoading(true);
+
+    //         setIsLoading(false);
+    //     }
+
+    //     fetchEvents();
+    // }, []);
+
+    // return (
+    //     <>
+    //         <div style={{ textAlign: 'center' }}>
+    //             {isLoading && <p>Loading...</p>}
+    //             {error && <p>{error}</p>}
+    //         </div>
+    //         {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    //     </>
+    // );
+    const events = useLoaderData();
+    return <EventsList events={events} />;
 }
 
 export default Events;
