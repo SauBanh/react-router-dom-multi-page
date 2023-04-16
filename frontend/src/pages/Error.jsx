e684ee27645c25b2519e95eb7e8fef8cfc117b48
SauBanh <saubanh05062001@gmail.com>
2023-04-16 15:19:37 +0700

    create page error to show error event or show error page not found

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+import PageContent from '../components/PageContent';
+import MainNavigation from '../components/MainNavigation';
 
 function Error() {
+    const error = useRouteError();
+    let title = 'An error occurred';
+    let message = 'Something went wrong.';
+    if (error.status === 500) {
+        // title = error.message
+        message = JSON.parse(error.data).message;
+    }
+    if (error.status === 404) {
+        title = 'Not Found!';
+        message = 'Could not find resource or page.';
+    }
     return (
         <>
-            <h1>Error</h1>
+            <MainNavigation />
+            <PageContent title={title}>
+                <p>{message}</p>
+            </PageContent>
         </>
     );
 }
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -39,10 +39,12 @@ function Events() {
 export default Events;
 
 export async function loader() {
-    const response = await fetch('http://localhost:8080/eventss');
+    const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
         // return { isError: true, message: 'Could not fetch events' };
+        // throw {message: 'could not fetch events'}
+        throw new Response(JSON.stringify({ message: 'could not fetch events' }), { status: 500 });
     } else {
         // const resData = await response.json();
         // const res = new Response('any data', {
